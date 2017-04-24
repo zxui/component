d72a6e8925a7f51e83f9ac4560a871512cb767ce
zxui <120656194@qq.com>
2017-04-24 21:46:28 +0800

    新增特效

--- demo/text/ad.js
+++ demo/text/ad.js
@@ -1,1 +1,9 @@
-let data=[]
+let data = ['JavaScript', 'Python', 'Ruby', 'C#', 'Go', 'Java', 'C++', 'Perl', 'Object-C', 'PHP', 'Groovy'];
+let level = 3;
+let strs = [];
+data.forEach((v, i)=> {
+    let fs = (Math.random() * 100).toFixed(0);
+    strs.push(`<span style="color: white;font-size:${fs}px " class="actor${i % 2 + 1}">${v}</span>`);
+    strs.push(`<br/>`);
+})
+document.getElementById('content').innerHTML = strs.join('');
